b77f07205f8fffe67d9a406ef4db7b5a034a780a
Arthur <hieuminhvuu@gmail.com>
2024-08-28 17:17:27 +0700

    feat(docs): add document

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Install-cluster-K8s
-Kubespray cài đặt cụm K8s một cách tự động, sử dụng bash script file
+# Hướng dẫn sử file script:
+
+## 1. Pre-Setup all vm
+- Xác định số node master - worker, setup ip, hostname, hosts
+file /etc/hosts của tất cả các node đều cần thông tin của chính nó và các node còn lại
+```
+sudo vim /etc/netplan/00-
+sudo vim /etc/hostname
+sudo vim /etc/hosts
+```
+- Sau khi đổi hostname, addhost xong thì reboot lại các vm 
+ssh tới các vm
+- tắt swap
+```
+swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+```
+
+## 2. Setup Master Node
+- su qua root user
+```
+sudo -s
+```
+- sử dụng git clone project này về
+- vim vào file script.sh sửa những thông số sau:
+  - USER : user mặc định khi cài vm mới, ví dụ như ubuntu, tránh sử dụng trực tiếp user root
+  - KUBESPRAY_VERSION : phiên bản kubespray muốn cài "release-2.25"
+  - PASSWORD="password"
+  - NODES=("your-node-ip-1" "your-node-ip-2" "your-node-ip-3")
+- chạy file bash script để quá trình tự động chạy, thời gian mất khoảng 20-25p hoặc có thể hơn tuỳ vào độ trễ mạng.
+```
+bash script.sh
+```
---
+++ script.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USER="ubuntu"
+KUBESPRAY_VERSION="release-2.25"
+PASSWORD="password"
+NODES=("your-node-ip-1" "your-node-ip-2" "your-node-ip-3")
+
+# Update system and install necessary packages
+sudo apt-get update
+sudo apt-get install -y python3-virtualenv git sshpass ansible-core
+
+# Create Python virtual environment
+virtualenv .kubespray
+source .kubespray/bin/activate
+
+# Clone Kubespray repository
+git clone https://github.com/kubernetes-sigs/kubespray.git --branch $KUBESPRAY_VERSION
+
+# Install Kubespray dependencies
+cd kubespray
+pip install -r requirements.txt
+
+# Generate ssh-key
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# Đường dẫn tới file khóa SSH
+KEY_PATH="${HOME}/.ssh/id_rsa.pub"
+
+for NODE in "${NODES[@]}"; do
+  # Kiểm tra nếu khóa SSH đã tồn tại trên node từ xa
+  echo "Checking if SSH key exists on $NODE..."
+  sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no $USER@$NODE "grep -q \"$(cat $KEY_PATH)\" ~/.ssh/authorized_keys"
+
+  # Nếu khóa chưa tồn tại, thì thêm nó
+  if [ $? -ne 0 ]; then
+    echo "Key not found on $NODE. Adding key..."
+    sshpass -p $PASSWORD ssh-copy-id -o StrictHostKeyChecking=no $USER@$NODE
+  else
+    echo "Key already exists on $NODE"
+  fi
+
+  # Kiểm tra xem có thể SSH mà không cần mật khẩu không
+  echo "Testing SSH login to $NODE..."
+  ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no $USER@$NODE "echo 'SSH login successful to $NODE without password'"
+  if [ $? -ne 0 ]; then
+    echo "Failed to SSH into $NODE without password."
+  else
+    echo "SSH into $NODE without password was successful."
+  fi
+done
+
+# Config
+cp -rfp inventory/sample inventory/mycluster
+CONFIG_FILE=inventory/mycluster/hosts.yaml python3 contrib/inventory_builder/inventory.py ${NODES[@]}
+
+# Delpoy
+ansible-playbook -i inventory/mycluster/hosts.yaml --become --become-user=root cluster.yml -u $USER --ask-become-pass
+
+#Extend cert to 10 years
+wget https://raw.githubusercontent.com/yuyicai/update-kube-cert/master/update-kubeadm-cert.sh
+chmod 755 update-kubeadm-cert.sh
+./update-kubeadm-cert.sh master --cri docker
+
+#Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Install Ingress controller by Helm
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --set controller.service.type=NodePort
+
